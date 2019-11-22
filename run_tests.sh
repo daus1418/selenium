cf6cd0c5c829773ee9e822cfbf8362b2a6d42ad5
David Useche <superdaus14@gmail.com>
2019-11-22 12:12:32 -0500

    cambios en el .sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,4 @@ docker run --rm \
            -v "$PWD/reports":/reports \
            --security-opt seccomp:unconfined \
            --shm-size "256M" \
-           ypasmk/robot-framework
+           daus1418/selenium.v1.0
